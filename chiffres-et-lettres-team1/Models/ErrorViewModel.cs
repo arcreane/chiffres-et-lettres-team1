8061c594f703ba2d59f5613761aea9c9dcd8bae2
jwnzao <jeff@aprentiv.com>
2021-07-08 15:32:44 +0200

    Ajoutez des fichiers projet.

---
+++ chiffres-et-lettres-team1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace chiffres_et_lettres_team1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
